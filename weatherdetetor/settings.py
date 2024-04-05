8c0b739beeca624f9212e4c8f8684c3924d7a1b7
Thomas Vu <samduykhanh01@gmail.com>
2024-04-05 19:09:52 -0400

    LOL

--- weatherdetetor/settings.py
+++ weatherdetetor/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 'weather-detector-eg5h.onrender.com/'
+                 'weather-detector-eg5h.onrender.com'
 ]
 
 
